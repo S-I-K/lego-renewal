552e06962690b25dcf8dd3b42c1e271fed7967c7
sik <daesik99@naver.com>
2022-12-29 18:48:14 +0900

    update/ search-bar, scroll animation add

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 $(() => {
+  /* swiper.js */
   /* theme category */
   var theme = new Swiper("#theme-category-swiper", {
     loop: true,
@@ -21,7 +22,6 @@ $(() => {
     pagination: {
       el: ".new-lego-pagination",
       clickable: true,
-      // dynamicBullets: true,
     },
     navigation: {
       nextEl: ".swiper-button-next",
@@ -39,6 +39,7 @@ $(() => {
     },
     pagination: {
       el: ".swiper-pagination",
+      type: "progressbar",
       clickable: true,
     },
   });
@@ -51,5 +52,83 @@ $(() => {
       delay: 5000,
       disableOnInteraction: false,
     },
+    pagination: {
+      el: ".marvel-pagination",
+      type: "progressbar",
+      clickable: true,
+    },
+  });
+
+  /*************  scrollToGiveClass *************/
+  /* header */
+  $("header").scrollToGiveClass({
+    class: "fixed",
+    baseline: 80,
+  });
+  /* category */
+  $(".theme-category .title").scrollToGiveClass({
+    class: "scrolled",
+    baseline: "middle",
+    add: 300,
+  });
+  $("#theme-category-swiper").scrollToGiveClass({
+    class: "scrolled",
+    baseline: "middle",
+    add: 300,
+  });
+  $(".new-logos h2.title").scrollToGiveClass({
+    class: "scrolled",
+    baseline: "middle",
+    add: 300,
+  });
+  $("#new-logo-swiper").scrollToGiveClass({
+    class: "scrolled",
+    baseline: "middle",
+    add: 300,
+  });
+  $(".mario-lego-wrap h2.title").scrollToGiveClass({
+    class: "scrolled",
+    baseline: "middle",
+    add: 300,
+  });
+  $("#mario-swiper").scrollToGiveClass({
+    class: "scrolled",
+    baseline: "middle",
+    add: 300,
+  });
+  $(".mario-lego-wrap .thumb-wrap").scrollToGiveClass({
+    class: "scrolled",
+    baseline: "middle",
+    add: 300,
+  });
+  $(".marvel-lego-wrap h2.title").scrollToGiveClass({
+    class: "scrolled",
+    baseline: "middle",
+    add: 300,
+  });
+  $("#marvel-swiper").scrollToGiveClass({
+    class: "scrolled",
+    baseline: "middle",
+    add: 300,
+  });
+  $(".marvel-lego-wrap .thumb-wrap").scrollToGiveClass({
+    class: "scrolled",
+    baseline: "middle",
+    add: 300,
+  });
+  $(".lego-story h2.title").scrollToGiveClass({
+    class: "scrolled",
+    baseline: "middle",
+    add: 300,
+  });
+  $(".lego-story .story-thumb").scrollToGiveClass({
+    class: "scrolled",
+    baseline: "middle",
+    add: 300,
+  });
+  $(".lego-story .story-txt").scrollToGiveClass({
+    class: "scrolled",
+    baseline: "middle",
+    add: 300,
   });
 });
